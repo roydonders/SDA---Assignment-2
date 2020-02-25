a2b4b8c3e476c51e9fecca54c85a6472de588ba2
Ka Ho <kahoso12@gmail.com>
2020-02-25 12:25:59 +0100

    ex 1-2

---
+++ Exc 2.R
@@ -0,0 +1,27 @@
+setwd("C:/Users/Ka-Ho/Documents/R/R Workspace/SDA")
+source("sample2020.txt")
+source("functions_Ch3.txt")
+
+z<- c(sample2020$sample2020b)
+par(mfrow=c(1,3),"s")
+qqnorm(z,pch=20,col="blue",cex=1)
+qqline(z)
+abline(3.28,1.635, col="yellow")
+qqt(z,df=5,pch=20,col="blue",cex=1)
+qqline(z)
+abline(3.28,1.635, col="yellow")
+qqlaplace(z,pch=20,col="blue",cex=1)
+qqline(z)
+abline(3.28,1.635, col="yellow")
+
+gammsa<-(qgamma(seq(0,1,by=.01),2.1,scale = 1.8))
+ks.test(z,qgamma(x,2.1,scale = 1.8))
+plot(z,gammsa)
+chisq.test(z,(qgamma(seq(0,0.79,by=.01),2.1,scale = 1.8)))
+qqplot(z,(qgamma(seq(0,0.79,by=.01),2.1,scale = 1.8)))
+
+chisq.test(b,(qgamma(seq(0,0.79,by=.01),2.1,scale = 1.8)))
+chisq.test(z)
+
+
+           
--- exercise1.R
+++ exercise1.R
@@ -1,22 +1,90 @@
-setwd("C:\Users\timon\Documents\R\SDA\ass2")
+
+setwd("C:/Users/Ka-Ho/Documents/R/R Workspace/SDA")
 source("sample2020.txt")
-
-x=seq(0,1,by=.01)
-normal<- qnorm(x,mean=1,sd=sqrt(4))
-qt3 <-qt(x,3) 
-plot(normal)
-plot(qt3)
-plot(normal,qt3)
-plot(qt3,normal)
-
+source("functions_Ch3.txt")
+par(mfrow=c(2,2), pty="s")
+x=seq(0,1,by=0.01)
 snormal <- qnorm(x,mean=0,sd=1)
 uni<-qunif(x,min=1,max=2)
 plot(uni)
 plot(snormal)
-plot(uni,snormal)
+plot(uni,snormal,
+     main = "QQ-Plot Uniform with Standard Normal",
+     xlab = "Uniform(1,2)", ylab = "Standard Normal",
+     pch=20,
+     col="blue",
+     cex=1.0)
+
+normal<- qnorm(x,mean=1,sd=sqrt(4))
+qt3 <-qt(x,3) 
+plot(normal)
+plot(qt3)
+plot(normal,qt3,
+     main = "QQ-Plot Normal with t-distribution",
+     xlab = "Normal(1,4)", ylab = "t-distribution with df = 3",
+     pch=20,
+     col="green",
+     cex=1)
+plot(qt3,normal)
+
 
 t10<-qt(x,10)
 log<-qlnorm(x,meanlog = 0.1,sdlog = 0.4)
 plot(t10)
 plot(log)
-plot(t10,log)
+plot(t10,log,
+     main = "QQ-Plot t-distribution with Log normal",
+     xlab = "t-distribution with df = 10", ylab = "Log normal with meanlog = 0.1, sdlog = 0.4",
+     pch=20,
+     col="red",
+     cex=1)
+
+qqplot(uni,snormal)
+qqplot(seq(0,10,by=0.01),y)
+
+y <- sort(sqrt(sample2020$sample2020a))
+yy <- sort(sample2020$sample2020a)
+plot(y)
+hist(y)
+qqlaplace(y)
+plot(yy)
+hist(yy)
+par(mfrow=c(1,3),"s")
+qqnorm(y,
+       pch=20,
+       col="blue",
+       cex=1)
+qqline(y)
+abline(1.871,0.927, col="yellow")
+qqt(y,df=3,
+    
+    pch=20,
+    col="blue",
+    cex=1)
+qqline(y)
+abline(1.871,0.927,col="yellow")
+
+qqlaplace(y,
+          pch=20,
+          col="blue",
+          cex=1)
+qqline(y)
+abline(1.871,0.928,col="yellow")
+
+hist(slope)
+qqline(y)
+qqnorm(yy)
+qqlnorm(y)
+qqplot(x,y)
+par(mfrow=c(2,2))
+
+qqnorm(y)
+
+qqlaplace(y,
+          pch=20,
+          col="green",
+          cex=1)
+qqline(y)
+abline(1.879,0.928,col="yellow")
+
+
