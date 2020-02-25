ea7ede6a7cbb9907167acac3a2e58148798a3c1b
Ka Ho <kahoso12@gmail.com>
2020-02-25 16:48:29 +0100

    ks test veranderd

--- Exc 2.R
+++ Exc 2.R
@@ -5,7 +5,7 @@ source("functions_Ch3.txt")
 
 z<- c(sample2020$sample2020b)
 par(mfrow=c(1,3),"s")
-qqnorm(z,pch=20,col="blue",cex=1)
+qqnorm(z,pch=20,col="blue",cex=1,ylab="Sorted Data")
 qqline(z)
 abline(3.28,1.635, col="yellow")
 qqt(z,df=5,pch=20,col="blue",cex=1)
@@ -20,12 +20,14 @@ x_gamma = seq(0,1,length.out = zlength)
 y_gamma = qgamma(x_gamma, 2.1, scale = 1.8)
 ks.test(z,y_gamma)
 plot(z,y_gamma)
+#volgens mij moet deze zo:
+ks.test(z,pgamma,2.1,scale =1.8)
 
 chisq.test(z,y_gamma)
 qqplot(z,y_gamma)
-
+hist(y_gamma)
 chisq.test(z,y_gamma)
 chisq.test(z)
-
+chisq.test(y_gamma)
 
            
