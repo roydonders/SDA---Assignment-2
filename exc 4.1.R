fdc7ce072c839256bddcc7d73a22316e9265b44b
Ka Ho <kahoso12@gmail.com>
2020-03-11 17:20:49 +0100

    start 4

---
+++ exc 4.1.R
@@ -0,0 +1,26 @@
+setwd("/Users/Ko/Documents/SDA---Assignment-2")
+source("functions_Ch3.txt")
+source("functions_Ch5.txt")
+data31 = read.table("birthweight.txt")
+bw = c(data31$V1)
+hist(bw)
+median(bw)
+
+#unknown distribution so emperical bootstrap
+B=1000
+ebw_TStar = numeric(B)
+for(i in 1:B){
+  exstar= sample(bw[],replace = TRUE)
+  ebw_TStar[i] = median(exstar)
+}
+hist(ebw_TStar)
+sd(ebw_TStar)
+
+#parametric bootstrap
+pbw_TStar = numeric(B)
+for(i in 1:B){
+  pxstar = rexp(bw[],rate = (1+ mean(bw)))
+  pbw_TStar[i] = median(pxstar)
+}
+hist(pbw_TStar)
+sd(pbw_TStar) #sd is heel laag(ERROR lecture 5)
