97fc10e8b968285182f5c8ed7360bf4f5870adab
Unknown <timonotter@gmail.com>
2020-03-18 20:59:52 +0100

    4.3 begin

--- exc 43.R
+++ exc 43.R
@@ -1,2 +1,9 @@
-wd = getwd()
-setwd(wd)
+setwd("C:/Users/timon/SDA---Assignment-2")
+source("light.txt")
+v1879 = light$`1879`
+v1882 = light$`1882`
+
+n1879 = length(v1879)
+
+
+n1882 = length(v1882)
