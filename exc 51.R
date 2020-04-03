e9bd19b85f9f4a03cd111cb01087225c401ba3b8
Unknown <timonotter@gmail.com>
2020-04-03 18:01:07 +0200

    51 en doc tm b

--- exc 51.R
+++ exc 51.R
@@ -1,4 +1,13 @@
 #setwd("/Users/Ko/Documents/SDA---Assignment-2")
 setwd("C:/Users/timon/SDA---Assignment-2")
 grades = scan("statgrades.txt")
+n = length(grades)
+grades==6
+adjgrades = grades[-26]
+t = sum(adjgrades>6)
+binom.test(t,n-1,alternative = "g")
 
+grades==6.5
+t = sum(grades>6.5)
+binom.test(t,n,alternative = "g")
+binom.test(t,n,alternative = "l")
