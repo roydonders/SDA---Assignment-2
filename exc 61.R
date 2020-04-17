dc983d70e6945ab87cd8372b05fc0e5699b06b84
Unknown <timonotter@gmail.com>
2020-04-17 11:02:20 +0200

    mini begin 6.1

--- exc 61.R
+++ exc 61.R
@@ -1,2 +1,8 @@
 #setwd("/Users/Ko/Documents/SDA---Assignment-2")
 setwd("C:/Users/timon/SDA---Assignment-2")
+edata = read.table("expensescrime.txt", header = TRUE, stringsAsFactors = FALSE) 
+#part a
+edatanostates = subset(edata, select=-c(state))
+head(edatanostates)
+pairs(edatanostates)
+
