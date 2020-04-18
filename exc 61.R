3547fe3bd6b4c037fda96332d04b5cc046f139d8
Unknown <timonotter@gmail.com>
2020-04-18 14:39:07 +0200

    verder aan 6.1

--- exc 61.R
+++ exc 61.R
@@ -5,4 +5,13 @@ edata = read.table("expensescrime.txt", header = TRUE, stringsAsFactors = FALSE)
 edatanostates = subset(edata, select=-c(state))
 head(edatanostates)
 pairs(edatanostates)
+#b
+plot(lawyerrate,edata$crime, xlab="Lawyer rate (per 100,000)", ylab="Crime rate (per 100,000)", main="Scatterplot of lawyer and crime rate")
+outliercrimestate_i = which.max(edata$crime)
+outliercrimestate = edata$state[outliercrimestate_i]
+outliercrimestate
 
+plot(lawyerrate,edata$crime, xlim=c(0,500), xlab="Lawyer rate (per 100,000)", ylab="Crime rate (per 100,000)", main="Scatterplot of lawyer and crime rate", sub="Zoomed in (lawyer rate max limit is 500)")
+n = length(lawyerrate)
+cor.test(lawyerrate,edata$crime, method="s")
+cor.test(lawyerrate,edata$crime, method="k")
