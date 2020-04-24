d827b957fbb640dc364f464f4a9dd55ce54383d4
Unknown <timonotter@gmail.com>
2020-04-24 17:50:13 +0200

    verder aan 6.1 alleen f nog

--- exc 61.R
+++ exc 61.R
@@ -6,6 +6,7 @@ edatanostates = subset(edata, select=-c(state))
 head(edatanostates)
 pairs(edatanostates)
 #b
+lawyerrate = (edata$lawyers*100)/edata$pop
 plot(lawyerrate,edata$crime, xlab="Lawyer rate (per 100,000)", ylab="Crime rate (per 100,000)", main="Scatterplot of lawyer and crime rate")
 outliercrimestate_i = which.max(edata$crime)
 outliercrimestate = edata$state[outliercrimestate_i]
@@ -15,3 +16,16 @@ plot(lawyerrate,edata$crime, xlim=c(0,500), xlab="Lawyer rate (per 100,000)", yl
 n = length(lawyerrate)
 cor.test(lawyerrate,edata$crime, method="s")
 cor.test(lawyerrate,edata$crime, method="k")
+
+B = 1000
+T = function(xs,ys) cor.test(xs,ys,method="k")$estimate
+t = T(lawyerrate,edata$crime)
+permutationtval = numeric(B)
+
+for(i in 1:B)
+{
+  permutationtval[i] = T(lawyerrate,sample(edata$crime))
+}
+pl = sum(permutationtval<=t)/B
+pr = sum(permutationtval>=t)/B
+pperm = 2*min(pl,pr)
